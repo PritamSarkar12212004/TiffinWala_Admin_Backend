dc3a9f7e60616d13e1cd888e5bc644abb22babcf
Pritam Sarkar <you@example.com>
2025-07-23 18:51:04 +0530

    auth done init

---
+++ src/controllers/auth/CreateprofileController.js
@@ -0,0 +1,41 @@
+import adminModel from "../../models/adminModel.js";
+
+const CreateprofileController = async (req, res) => {
+  const { Description, Name, email, location, profileImage, phone, gender } =
+    req.body;
+  if (
+    !Description ||
+    !Name ||
+    !email ||
+    !location ||
+    !profileImage ||
+    !phone ||
+    !gender
+  ) {
+    return res.status(400).json({ message: "All fields are required" });
+  } else {
+    // console.log(Description, Name, email, location, profileImage, phone);
+    const createProfile = await adminModel
+      .create({
+        User_Name: Name,
+        User_Image: profileImage,
+        User_Email: email,
+        User_Phone_Number: phone,
+        User_Bio: Description,
+        User_Address: location,
+        User_Gender: gender,
+      })
+      .then((result) => {
+        console.log(result);
+        return res.status(201).json({
+          message: "Profile created successfully",
+          data: result,
+        });
+      })
+      .catch((err) => {
+        console.error(err);
+        return res.status(500).json({ message: "Error creating profile" });
+      });
+  }
+};
+export default CreateprofileController;
